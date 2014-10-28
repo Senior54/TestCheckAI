a11976fa1c7b548c6d8784fbe6a1eb74d76310ec
Jennifer Senior <senior54@students.rowan.edu>
2014-10-28 12:36:09 -0400

    Adding two more printlns in class main about changes so far.

--- src/testPackage/TestClassMain.java
+++ src/testPackage/TestClassMain.java
@@ -4,8 +4,11 @@ public class TestClassMain {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World! I am here> :) ");
+		System.out.println("Hello World! I am here. :) ");
 
+		System.out.println("First change was adding package and" +
+				"a main class with simple println.");
+		System.out.println("Second change was just adding more printlns.");
 	}
 
 }
